b1bde1b9c27e83796beeec0f53f51a679a2db239
DNature <divinehycenth@outlook.com>
2020-04-20 18:00:56 +0100

    added form submissions to netlify

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,64 +1,104 @@
 import React from "react";
 import { NextPage } from "next";
 import { IoIosMail, IoMdCall, IoMdPin } from "react-icons/io";
+import { useRouter } from 'next/router';
 
 import MainLayout from "../layouts/Main";
 
-const ContactPage: NextPage = () => (
-  <MainLayout pathname="/contact">
-    <div className="w-full gradient relative -z-10  after after-2">
-      <div className="w-full h-full overflow-hidden absolute top-0 left-0 -z-5">
-        <div className="gradient-alt -rotate-180  circle absolute" />
-        <div className="gradient-alt circle-2 absolute" />
-      </div>
-      <div className="container py-32 sm:py-40 md:py-40 z-20">
-        <h1 className="text-center text-xl sm:text-3xl lg:text-4xl font-bold text-white">I’d Love to hear from you</h1>
-        <p className="pb-56 text-center font-bold text-gray-100">Contact me using the form bellow and i’ll get back to you right away</p>
-      </div>
-    </div>
+const encode = (data: any): string => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
 
-    <div className="container -mt-64">
-      <div className="block lg:grid grid-cols-6 gap-6 grid-rows-2 xl:px-40 lg:px-32">
-        <div className="card-contact shadow rounded-lg col-span-3 row-span-6 p-5 row-span-1 row-end-3">
-          <h2 className="text-xl xl:text-2xl  g100 font-bold">Fill out the form and i’ll be in touch as soon as possible.</h2>
 
-          <form data-netlify="true">
-            <div className="my-8">
-              <input type="text" className="p-6 bg-g-15 rounded-sm w-full outline-none" placeholder="Full Name" autoFocus />
-            </div>
-            <div className="my-8">
-              <input type="text" className="p-6 bg-g-15 rounded-sm w-full outline-none" placeholder="Full Name" />
-            </div>
-            <div className="my-8">
-              <textarea className="p-6 bg-g-15 rounded-sm w-full h-40 outline-none resize-none" placeholder="How can i help?..." />
-            </div>
+const ContactPage: NextPage = () => {
+  
+  const router = useRouter();
+  const [values, setValues] = React.useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-            <button type="submit" className="btn py-3 w-full shadow-primary font-bold rounded-full hover:opacity-75 mb-8 mt-10">
-              Send
-            </button>
-          </form>
+  const handleChange = (e: any): void => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: any): void => {
+    e.preventDefault();
+    console.log(values);
+
+    fetch("/contact", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "Contact from divinehycenth.com", ...values }),
+    })
+      .then(() => {
+        console.log("message sent");
+        return setTimeout(() => {
+          return router.push("/sent");
+        }, 1000);
+      })
+      .catch((e) => console.log(e));
+    // TODO: Create a success redirect page
+  };
+
+  return (
+    <MainLayout pathname="/contact">
+      <div className="w-full gradient relative -z-10  after after-2">
+        <div className="w-full h-full overflow-hidden absolute top-0 left-0 -z-5">
+          <div className="gradient-alt -rotate-180  circle absolute" />
+          <div className="gradient-alt circle-2 absolute" />
         </div>
-        <div className="col-span-3 row-end-3 mt-20 lg:mt-0">
-          <div className="gradient shadow-primary shadow rounded-lg mt-auto h-full p-6">
-            <img src="/images/logo.svg" alt="Logo" className="mb-8" />
+        <div className="container py-32 sm:py-40 md:py-40 z-20">
+          <h1 className="text-center text-xl sm:text-3xl lg:text-4xl font-bold text-white">I’d Love to hear from you</h1>
+          <p className="pb-56 text-center font-bold text-gray-100">Contact me using the form bellow and i’ll get back to you right away</p>
+        </div>
+      </div>
 
-            <div className="text-white flex">
-              <IoIosMail className="text-white text-xl lg:text-2xl lg:mr-7" />
-              <span className="ml-6 lg:text-lg">divinehycenth@outlook.com</span>
-            </div>
-            <div className="text-white flex  my-6">
-              <IoMdCall className="text-white text-xl lg:text-2xl lg:mr-7" />
-              <span className="ml-6 lg:text-lg">+234 8123 161 019</span>
-            </div>
-            <div className="text-white flex">
-              <IoMdPin className="text-white text-xl lg:text-2xl lg:mr-7" />
-              <span className="ml-6 lg:text-lg">Gwagwalada, Abuja - Nigeria</span>
+      <div className="container -mt-64">
+        <div className="block lg:grid grid-cols-6 gap-6 grid-rows-2 xl:px-40 lg:px-32">
+          <div className="card-contact shadow rounded-lg col-span-3 row-span-6 p-5 row-span-1 row-end-3">
+            <h2 className="text-xl xl:text-2xl  g100 font-bold">Fill out the form and i’ll be in touch as soon as possible.</h2>
+
+            <form name="Contact form for divinehycenth.com" onSubmit={handleSubmit}>
+              <div className="my-8">
+                <input type="text" onChange={handleChange} className="p-6 bg-g-15 rounded-sm w-full outline-none" required placeholder="Full Name" autoFocus value={values.name} />
+              </div>
+              <div className="my-8">
+                <input type="text" onChange={handleChange} className="p-6 bg-g-15 rounded-sm w-full outline-none" required placeholder="Email" value={values.email} />
+              </div>
+              <div className="my-8">
+                <textarea onChange={handleChange} className="p-6 bg-g-15 rounded-sm w-full h-40 outline-none resize-none" required placeholder="How can i help?..." value={values.message} />
+              </div>
+
+              <button type="submit" className="btn py-3 w-full shadow-primary font-bold rounded-full hover:opacity-75 mb-8 mt-10">
+                Send
+              </button>
+            </form>
+          </div>
+          <div className="col-span-3 row-end-3 mt-20 lg:mt-0">
+            <div className="gradient shadow-primary shadow rounded-lg mt-auto h-full p-6">
+              <img src="/images/logo.svg" alt="Logo" className="mb-8" />
+
+              <div className="text-white flex">
+                <IoIosMail className="text-white text-xl lg:text-2xl lg:mr-7" />
+                <span className="ml-6 lg:text-lg">divinehycenth@outlook.com</span>
+              </div>
+              <div className="text-white flex  my-6">
+                <IoMdCall className="text-white text-xl lg:text-2xl lg:mr-7" />
+                <span className="ml-6 lg:text-lg">+234 8123 161 019</span>
+              </div>
+              <div className="text-white flex">
+                <IoMdPin className="text-white text-xl lg:text-2xl lg:mr-7" />
+                <span className="ml-6 lg:text-lg">Gwagwalada, Abuja - Nigeria</span>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </MainLayout>
-);
+    </MainLayout>
+  );};
 
 export default ContactPage;
