5d6856d3fdfdabc0099c2f4b74724faabf2e690a
DNature <divinehycenth@outlook.com>
2020-04-25 18:29:59 +0100

    changed mouse cursor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { AppProps } from "next/app";
 import Router from "next/router";
 import NProgress from "nprogress";
 
-process.env.NODE_ENV === "development" ? require("../styles/bundle.css") : require("../styles/styles.css");
+process.env.NODE_ENV === "development" ? require("../styles/styles.css") : require("../styles/bundle.css");
 import "../styles/progress.css";
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
